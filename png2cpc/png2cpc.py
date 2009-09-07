0f1f9adcf6e9b03784b24493ed6cd189ba47900a
Mauricio Mu?oz Lucero <real.mml@gmail.com>
2009-09-07 10:16:18 +0200

    A?adimos la inversi?n y los modos de bloques continuos.

--- png2cpc/png2cpc.py
+++ png2cpc/png2cpc.py
@@ -12,6 +12,7 @@ png2cpc
 # - ¿Añadir colores a la paleta hasta tener los 16?
 # - Solo acepta los 16 primeros colores y no muestra errores
 # - Si no das la opción -p convierte la imagen aunque no use la paleta cpc
+# - Añadir el remapeado de la paleta externa a la interna.
 
 # Parches para que funcione en python 2.5
 from __future__ import with_statement
@@ -31,34 +32,64 @@ pixels_por_byte = (2, 4, 8)
 
 # Paleta del CPC
 # Cadena RGB : Tinta + Color Hardware (lista para el Gate Array)
-paleta_cpc = {
-    '\x00\x00\x00' : 64 + 20,         # Negro
-    '\x00\x00\x7F' : 64 + 4,         # Azul
-    '\x00\x00\xFF' : 64 + 21,         # Azul Brillante
-    '\x7F\x00\x00' : 64 + 28,         # Rojo
-    '\x7F\x00\x7F' : 64 + 24,         # Magenta
-    '\x7F\x00\xFF' : 64 + 29,         # Malva
-    '\xFF\x00\x00' : 64 + 12,         # Rojo Brillante
-    '\xFF\x00\x7F' : 64 + 5,         # Purpura
-    '\xFF\x00\xFF' : 64 + 13,         # Magenta Brillante
-    '\x00\x7F\x00' : 64 + 22,         # Verde
-    '\x00\x7F\x7F' : 64 + 6,         # Cian
-    '\x00\x7F\xFF' : 64 + 23,         # Azul Cielo
-    '\x7F\x7F\x00' : 64 + 30,         # Amarillo
-    '\x7F\x7F\x7F' : 64 + 0,         # Blanco
-    '\x7F\x7F\xFF' : 64 + 31,         # Azul Pastel
-    '\xFF\x7F\x00' : 64 + 14,         # Naranja
-    '\xFF\x7F\x7F' : 64 + 7,         # Rosa
-    '\xFF\x7F\xFF' : 64 + 15,         # Magenta Pastel
-    '\x00\xFF\x00' : 64 + 18,         # Verde Brillante
-    '\x00\xFF\x7F' : 64 + 2,         # Verde Mar
-    '\x00\xFF\xFF' : 64 + 19,         # Cian Brillante
-    '\x7F\xFF\x00' : 64 + 26,         # Lima
-    '\x7F\xFF\x7F' : 64 + 25,         # Verde Pastel
-    '\x7F\xFF\xFF' : 64 + 27,         # Cian Pastel
-    '\xFF\xFF\x00' : 64 + 10,         # Amarillo Brillante
-    '\xFF\xFF\x7F' : 64 + 3,         # Amarillo Pastel
-    '\xFF\xFF\xFF' : 64 + 11         # Blanco Brillante
+paleta2cpc = {
+    '\x00\x00\x00' : 64 + 20,         # Negro               0x54 (84)
+    '\x00\x00\x7F' : 64 + 4,          # Azul                0x44 (68)
+    '\x00\x00\xFF' : 64 + 21,         # Azul Brillante      0x55 (85)
+    '\x00\x7F\x00' : 64 + 22,         # Verde               0x56 (86)
+    '\x00\x7F\x7F' : 64 + 6,          # Cian                0x46 (70)
+    '\x00\x7F\xFF' : 64 + 23,         # Azul Cielo          0x57 (87)
+    '\x00\xFF\x00' : 64 + 18,         # Verde Brillante     0x52 (82)
+    '\x00\xFF\x7F' : 64 + 2,          # Verde Mar           0x42 (66)
+    '\x00\xFF\xFF' : 64 + 19,         # Cian Brillante      0x53 (83)
+    '\x7F\x00\x00' : 64 + 28,         # Rojo                0x5c (92)
+    '\x7F\x00\x7F' : 64 + 24,         # Magenta             0x58 (88)
+    '\x7F\x00\xFF' : 64 + 29,         # Malva               0x5d (93)
+    '\x7F\x7F\x00' : 64 + 30,         # Amarillo            0x5e (94)
+    '\x7F\x7F\x7F' : 64 + 0,          # Blanco              0x40 (64)
+    '\x7F\x7F\xFF' : 64 + 31,         # Azul Pastel         0x5f (95)
+    '\x7F\xFF\x00' : 64 + 26,         # Lima                0x5a (90)
+    '\x7F\xFF\x7F' : 64 + 25,         # Verde Pastel        0x59 (89)
+    '\x7F\xFF\xFF' : 64 + 27,         # Cian Pastel         0x5b (91)
+    '\xFF\x00\x00' : 64 + 12,         # Rojo Brillante      0x4c (76)
+    '\xFF\x00\x7F' : 64 + 5,          # Purpura             0x45 (69)
+    '\xFF\x00\xFF' : 64 + 13,         # Magenta Brillante   0x4d (77)
+    '\xFF\x7F\x00' : 64 + 14,         # Naranja             0x4e (78)
+    '\xFF\x7F\x7F' : 64 + 7,          # Rosa                0x47 (71)
+    '\xFF\x7F\xFF' : 64 + 15,         # Magenta Pastel      0x4f (79)
+    '\xFF\xFF\x00' : 64 + 10,         # Amarillo Brillante  0x4a (74)
+    '\xFF\xFF\x7F' : 64 + 3,          # Amarillo Pastel     0x43 (67)
+    '\xFF\xFF\xFF' : 64 + 11          # Blanco Brillante    0x4b (75)
+}
+
+cpc2paleta = {
+    64 + 20 : '\x00\x00\x00',         # Negro
+    64 + 4  : '\x00\x00\x7F',         # Azul
+    64 + 21 : '\x00\x00\xFF',         # Azul Brillante
+    64 + 28 : '\x7F\x00\x00',         # Rojo
+    64 + 24 : '\x7F\x00\x7F',         # Magenta
+    64 + 29 : '\x7F\x00\xFF',         # Malva
+    64 + 12 : '\xFF\x00\x00',         # Rojo Brillante
+    64 + 5  : '\xFF\x00\x7F',         # Purpura
+    64 + 13 : '\xFF\x00\xFF',         # Magenta Brillante
+    64 + 22 : '\x00\x7F\x00',         # Verde
+    64 + 6  : '\x00\x7F\x7F',         # Cian
+    64 + 23 : '\x00\x7F\xFF',         # Azul Cielo
+    64 + 30 : '\x7F\x7F\x00',         # Amarillo
+    64 + 0  : '\x7F\x7F\x7F',         # Blanco
+    64 + 31 : '\x7F\x7F\xFF',         # Azul Pastel
+    64 + 14 : '\xFF\x7F\x00',         # Naranja
+    64 + 7  : '\xFF\x7F\x7F',         # Rosa
+    64 + 15 : '\xFF\x7F\xFF',         # Magenta Pastel
+    64 + 18 : '\x00\xFF\x00',         # Verde Brillante
+    64 + 2  : '\x00\xFF\x7F',         # Verde Mar
+    64 + 19 : '\x00\xFF\xFF',         # Cian Brillante
+    64 + 26 : '\x7F\xFF\x00',         # Lima
+    64 + 25 : '\x7F\xFF\x7F',         # Verde Pastel
+    64 + 27 : '\x7F\xFF\xFF',         # Cian Pastel
+    64 + 10 : '\xFF\xFF\x00',         # Amarillo Brillante
+    64 + 3  : '\xFF\xFF\x7F',         # Amarillo Pastel
+    64 + 11 : '\xFF\xFF\xFF'         # Blanco Brillante
 }
 
 # Código ejecutable para visualizar imagenes en el cpc
@@ -109,8 +140,8 @@ def extrae_paleta(fichero_imagen, modo_imagen):
             cantidad_de_colores = MAX_CPC_COLORS
         for i in range(cantidad_de_colores):
             paleta_aux = paleta_tmp[i*3] + paleta_tmp[i*3+1] + paleta_tmp[i*3+2]
-            if (paleta_aux in paleta_cpc):
-                paleta_final[paleta_aux] = (i, paleta_cpc[paleta_aux])
+            if (paleta_aux in paleta2cpc):
+                paleta_final[paleta_aux] = (i, paleta2cpc[paleta_aux])
             else:
                 paleta_final[paleta_aux] = (i, i)
                 paleta_cpc_valida = False
@@ -120,27 +151,64 @@ def extrae_paleta(fichero_imagen, modo_imagen):
             for i in range(len(paleta_tmp)):
                 paleta_aux = chr(paleta_tmp[i][1][0]) + \
                     chr(paleta_tmp[i][1][1]) + chr(paleta_tmp[i][1][2])
-                if (paleta_aux in paleta_cpc):
-                    paleta_final[paleta_aux] = (i, paleta_cpc[paleta_aux])
+                if (paleta_aux in paleta2cpc):
+                    paleta_final[paleta_aux] = (i, paleta2cpc[paleta_aux])
                 else:
                     paleta_final[paleta_aux] = (i, i)
                     paleta_cpc_valida = False
     else:
         print u"Modo no soportado."
+
+    return paleta_final, paleta_cpc_valida
+
+def convierte_paleta_externa(paleta):
+    """
+    Convierte una paleta externa a formato interno
+    """
+    paleta_final = {}
+    paleta_cpc_valida = True
+
+    for i in range(len(paleta)):
+        paleta_final[cpc2paleta[ord(paleta[i])]] = (i, ord(paleta[i]))
+
     return paleta_final, paleta_cpc_valida
         
-def extrae_imagen(fichero_imagen, modo_imagen, ancho_imagen, alto_imagen, paleta):
+def extrae_imagen(fichero_imagen, modo_imagen, ancho_imagen, alto_imagen, paleta, ancho_inversion, ppb): #, indice_paleta):
     """
     Extrae la imagen cruda de la imagen
     """
-    imagen_final = list(fichero_imagen.getdata())
+    imagen_tmp = list(fichero_imagen.getdata())
     if (modo_imagen=="RGB") or (modo_imagen=="RGBA"):
         for i in range(alto_imagen * ancho_imagen):
-            imagen_final[i] = paleta[chr(imagen_final[i][0]) + chr(imagen_final[i][1]) + chr(imagen_final[i][2])][0]
+            imagen_tmp[i] = paleta[chr(imagen_tmp[i][0]) + chr(imagen_tmp[i][1]) + chr(imagen_tmp[i][2])][0]
+#        imagen_tmp = [paleta[chr(imagen_tmp[i][0]) + chr(imagen_tmp[i][1]) + chr(imagen_tmp[i][2])][0] 
+#            for i in range(alto_imagen * ancho_imagen)]
     elif (modo_imagen != "P"):
-        imagen_final = []
+        imagen_tmp = []
         print u"Modo no soportado."
-    imagen_final = map(chr, imagen_final)
+
+#    if indice_paleta:
+#        imagen_tmp = [indice_paleta + i for i in imagen_tmp]
+
+    imagen_tmp = map(chr, imagen_tmp)
+
+    # FIX: Hay un problema en la inversión, cuando indice_paleta > 0
+    imagen_final = []
+    if (ancho_inversion):
+        ai_ppb = ancho_inversion * ppb
+        for i in range(0, len(imagen_tmp), ancho_imagen):
+            # Inversión de los graficos
+#            for j in reversed(range(ancho_imagen)):
+#                imagen_final += imagen_tmp[i + j]
+            for j in reversed(range(ancho_imagen // ai_ppb)):
+#                print len(imagen_tmp[i + j * ai_ppb: i + (j + 1) * ai_ppb]),
+                imagen_final += imagen_tmp[i + j * ai_ppb: i + (j + 1) * ai_ppb]
+    else:
+        imagen_final = imagen_tmp
+
+#    for i in range(len(imagen_final)):
+#        print '%x' % ord(imagen_final[i]),
+
     return imagen_final
 
 def extrae_mapa(imagen, ancho_patron, alto_patron, ancho_imagen, alto_imagen):
@@ -187,13 +255,12 @@ def optimiza_paleta(paleta):
     """
     Optimiza la paleta
     """
-    paleta_final = []
-    for i in range(len(paleta)):
-        paleta_final.append("")
+    paleta_final = [""] * len(paleta)
+
     for i in paleta.keys():
         paleta_final[paleta[i][0]] = chr(paleta[i][1])
     return paleta_final
-
+    
 def optimiza_mapa(mapa, patrones):
     """
     Optimiza el mapa
@@ -207,9 +274,8 @@ def optimiza_patrones(patrones, ppb):
     """
     Optimiza los patrones
     """
-    patrones_final = []
-    for i in range(len(patrones)):
-        patrones_final.append("")
+    patrones_final = [""] * len(patrones)
+
     for i in patrones.keys():
         patrones_final[patrones[i][1]] = convierte_graficos(patrones[i][0], ppb)
     return patrones_final
@@ -218,9 +284,8 @@ def optimiza_casillas(casillas):
     """
     Optimiza las casillas
     """
-    casillas_final = []
-    for i in range(len(casillas)):
-        casillas_final.append("")
+    casillas_final = [""] * len(casillas)
+
     for i in casillas.keys():
         casillas_final[casillas[i][1]] = casillas[i][0]
     return casillas_final
@@ -234,6 +299,8 @@ def convierte_graficos(cadena, ppb):
         if ppb == 2:
             byte_tmp0 = cadena[i * ppb]
             byte_tmp1 = cadena[i * ppb + 1]
+#            byte_tmp0 = ord(cadena[i * ppb]) # & 0x0F
+#            byte_tmp1 = ord(cadena[i * ppb + 1]) #& 0x0F
             # p1b3->b0 p1b2->b4
             # p1b1->b2 p1b0->b6
             # p0b3->b1 p0b2->b5
@@ -242,6 +309,10 @@ def convierte_graficos(cadena, ppb):
                         ((ord(byte_tmp1) & 0x02) << 1) | ((ord(byte_tmp1) & 0x01) << 6) | \
                         ((ord(byte_tmp0) & 0x08) >> 2) | ((ord(byte_tmp0) & 0x04) << 3) | \
                         ((ord(byte_tmp0) & 0x02) << 2) | ((ord(byte_tmp0) & 0x01) << 7))
+#            byte_tmp = chr(((byte_tmp1 & 0x08) >> 3) | ((byte_tmp1 & 0x04) << 2) | \
+#                        ((byte_tmp1 & 0x02) << 1) | ((byte_tmp1 & 0x01) << 6) | \
+#                        ((byte_tmp0 & 0x08) >> 2) | ((byte_tmp0 & 0x04) << 3) | \
+#                        ((byte_tmp0 & 0x02) << 2) | ((byte_tmp0 & 0x01) << 7))
         elif ppb == 4:
             byte_tmp0 = cadena[i * ppb]
             byte_tmp1 = cadena[i * ppb + 1]
@@ -375,18 +446,21 @@ def procesar_linea_comandos(linea_de_comandos):
     if linea_de_comandos is None:
         linea_de_comandos = sys.argv[1:]
 
-    version_programa = "%prog v0.4"
+    version_programa = "%prog v0.5"
     uso_programa = "usage: %prog [options] img1.png img2.png ... imgX.png"
     descripcion_programa = "%prog convert images in png format to binary data for Amstrad CPC."
 
     # definimos las opciones que soportaremos desde la lnea de comandos
     lista_de_opciones = [
         make_option("-e", "--exec", action="store_true", dest="executable", default=False, help="Generate executable"),
-        make_option("-s", "--sprite", action="store_true", dest="sprite", default=False, help="Generate sprites"),
-        make_option("-p", "--palette", action="store_true", dest="palette", default=False, help="Generate palettes"),
+        make_option("-i", "--inverse", action="store", type="int", dest="inverse_width", default=0, help="Select inverse mode width (12, 16, ...)"),
+#        make_option("-l", "--loadpal", action="store", dest="load_palette", default="", help="Load external palette"),
         make_option("-m", "--map", action="store_true", dest="map", default=False, help="Generate map"),
-        make_option("-t", "--tile", action="store_true", dest="tile", default=False, help="Generate tiles"),
+#        make_option("-n", "--index", action="store", type="int", dest="index", default=0, help="Select index begin palette (0-15)"),
+        make_option("-p", "--palette", action="store_true", dest="palette", default=False, help="Generate palettes"),
         make_option("-r", "--mode", action="store", type="int", dest="mode", default=0, help="Select screen mode (0, 1, 2)"),
+        make_option("-s", "--sprite", action="store_true", dest="sprite", default=False, help="Generate sprites"),
+        make_option("-t", "--tile", action="store_true", dest="tile", default=False, help="Generate tiles"),
         make_option("-X", "--map_width", action="store", type="int", dest="ancho_casilla", default=1, help="Tilemap width"),
         make_option("-Y", "--map_height", action="store", type="int", dest="alto_casilla", default=1, help="Tilemap height"),
         make_option("-x", "--tile_width", action="store", type="int", dest="ancho_patron", default=8, help="Tile width"),
@@ -436,15 +510,28 @@ def main(linea_de_comandos=None):
         # Dimensiones de la imagen
         ancho_imagen, alto_imagen = fichero_imagen.size
         print u"Tamaño en pixels de la imagen: %d x %d" % (ancho_imagen, alto_imagen)
-        ancho , alto = ancho_imagen // opciones.ancho_patron, alto_imagen // opciones.alto_patron
+        ancho, alto = ancho_imagen // opciones.ancho_patron, alto_imagen // opciones.alto_patron
         print u"Tamaño en patrones de la imagen: %d x %d" % (ancho, alto)
 
         # Características de la imagen
         modo_imagen = fichero_imagen.mode
         #print u"Modo de la imagen:", modo_imagen
 
-        # Generamos la paleta        
+        # Generamos la paleta
         paleta, paleta_cpc_valida = extrae_paleta(fichero_imagen, modo_imagen)
+
+#        if (opciones.load_palette == ""):
+#            paleta, paleta_cpc_valida = extrae_paleta(fichero_imagen, modo_imagen)
+#        else:
+#            if not(os.path.exists(opciones.load_palette)):
+#                print u"El fichero %s no existe." % opciones.load_palette
+#                opciones.load_palette = ""
+#                continue
+#            else:
+#                with open(opciones.load_palette,'r') as f:
+#                    paleta = f.read()
+#                    paleta, paleta_cpc_valida = convierte_paleta_externa(paleta)
+        
         print u"Número de colores: %d" % len(paleta)
 
         if (opciones.palette and (not opciones.executable)):
@@ -455,7 +542,7 @@ def main(linea_de_comandos=None):
 
         if not opciones.sprite:
             fichero_imagen.load()    # Cargamos el fichero en memoria
-            imagen = extrae_imagen(fichero_imagen, modo_imagen, ancho_imagen, alto_imagen, paleta)
+            imagen = extrae_imagen(fichero_imagen, modo_imagen, ancho_imagen, alto_imagen, paleta, opciones.inverse_width, pixels_por_byte[opciones.mode]) #, opciones.index)
             if not (opciones.map or opciones.tile):
                 imagen = convierte_graficos(imagen, pixels_por_byte[opciones.mode])        # Se convierten los bytes a formato cpc
                 if (opciones.executable):
